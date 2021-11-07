856a5e52d9cd67a7ab390bd2090a712756954d85
ENG19CS0047-ASHISH <76139721+ENG19CS0047-ASHISH@users.noreply.github.com>
2021-11-07 15:14:31 +0530

    Add files via upload

---
+++ KNearestNeighbours.py
@@ -0,0 +1,14 @@
+#K nearest neighbours
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+X = [[30], [40], [50], [60], [20], [10], [70]]
+Y = [0, 1, 1, 1, 0, 0, 1]
+# Here 0 means fail and 1 means pass
+classifier = KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2)
+classifier.fit(X, Y)
+a = int(input("Enter marks "))
+X_marks = [[a]]
+print(classifier.predict(X_marks))
---
+++ README.md
@@ -0,0 +1,5 @@
+# -Deep-Learning-Assignment_01
+## Abhishek Krishna TM
+## ENG19CS0011
+## Colab Link Below
+## https://colab.research.google.com/drive/1fLTJBD4ROy2SFWIWbqflIj2rjj_8wYMA#scrollTo=TN4Ve8-KjGT1
